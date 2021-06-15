03c3da66b76dffa3834287d1416ec47d4fc22256
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-15 02:06:00 -0400

    buyCake action is created

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,1 +1,10 @@
-console.log(`From index.js`)
+const buy_cake = "buy_cake";
+
+
+// first step is to create an action, which is an object that has type property
+function buyCake() {
+    return {
+        type: buy_cake,
+        info: "first redux action"
+    }
+}
