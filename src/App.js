a6e0759f8b3a1585ab6b6496a33567ff4efcb558
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-29 01:31:18 -0400

    IceCreamContainer component is created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Provider store={store}>
         <HooksCakeContainer />
         <CakeContainer />
+        <IceCreamContainer />
       </Provider>
     </>
   );
---
+++ src/components/IceCreamContainer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { buyIceCream } from '../redux'
+// to connet state to react component, we use connect
+import { connect } from 'react-redux'
+
+
+function IceCreamContainer(props) {
+    return (
+        <div>
+            <h2>Number of ice creams - {props.numOfIceCream}</h2>
+            <button onClick={props.buyIceCream}>Buy Ice Cream</button>
+        </div>
+    )
+}
+
+// function that takes state as parameter and return object - to get redux state 
+const mapStateToProps = state => {
+    return {
+        numOfIceCream: state.numOfIceCream
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyIceCream: () => dispatch(buyIceCream())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IceCreamContainer)
