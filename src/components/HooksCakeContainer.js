93903f0296c504b2580aca6fd26081dbd36b056d
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-29 00:17:26 -0400

    useSelector is used to get redux state

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 export default function HooksCakeContainer() {
+    const numOfCakes = useSelector(state => state.numOfCakes)
     return (
         <div>
-            <h2>Number of Cakes - </h2>
+            <h2>Number of Cakes - {numOfCakes}</h2>
             <button>Buy Cake</button>
         </div>
     )
