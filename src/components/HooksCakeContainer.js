7cb617f4125c1fbce6b397ed6159268d2d9b31be
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-28 23:53:12 -0400

    HooksCakeContainer is added

---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function HooksCakeContainer() {
+    return (
+        <div>
+            <h2>Number of Cakes - </h2>
+            <button>Buy Cake</button>
+        </div>
+    )
+}
