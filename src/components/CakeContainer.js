bf404ccf61972c560a1022b6087adfa8a7a52aef
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-28 23:34:05 -0400

    complete example of redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { buyCake } from '../redux'
+// to connet state to react component, we use connect
+import { connect } from 'react-redux'
 
-function CakeContainer() {
+
+function CakeContainer(props) {
     return (
         <div>
-            <h2>Number of cakess</h2>
-            <button>Buy Cake</button>
+            <h2>Number of cakess - {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     )
 }
 
-export default CakeContainer
+// function that takes state as parameter and return object - to get redux state 
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
