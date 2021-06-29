0f614100c09c0141880ea0ba2f73e430fa497243
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-28 22:00:52 -0400

    provider is added in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+// to link react with store.js 
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
     <>
-      <CakeContainer />
+      <Provider store={store}>
+        <CakeContainer />
+      </Provider>
     </>
   );
 }
